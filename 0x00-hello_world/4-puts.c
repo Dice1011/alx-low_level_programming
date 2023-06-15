df913f3529beb9b32107a3f2e1dee7da872d823e
Dice1011 <u12283593@tuks.co.za>
2023-06-15 07:13:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* Programming is like a multilingual puzzel */
+/*program returns 0*/
+
+	int main()
+	{
+		puts("Programming is like building a multilingual puzzle\n");
+		return 0;
+	}
