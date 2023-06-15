597b3f5c04baa9a9cd7be51b64fb70c6b2560f4a
Dice1011 <u12283593@tuks.co.za>
2023-06-15 08:04:26 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - with proper grammar, but the outcome is a piece of art,
+ *
+ * Descriptin: Program uses printf funtion to display standard output
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
