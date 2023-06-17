b44a552ce6d59d7e8ce688638bfde054786de7db
Dice1011 <u12283593@tuks.co.za>
2023-06-17 02:11:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char sentance[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, sentance, 59);
 	return (1);
 }
