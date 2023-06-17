5f6cd94fa7eef29782bd4397eeb1494108aa8156
Dice1011 <u12283593@tuks.co.za>
2023-06-17 01:59:40 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints a string
+ *
+ * Description : Prgram uses the write function to print STD output
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
