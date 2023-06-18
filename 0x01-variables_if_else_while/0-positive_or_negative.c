b76ffcc2fdb882d8a8839a5b2435aa98b908e5c0
Dice1011 <u12283593@tuks.co.za>
2023-06-18 15:05:59 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,16 +17,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (n == 0)
 	{
-		printf("%d : is positive\n", n);
+		printf("%d : is zero\n", n);
 	}
 	if (n < 0)
 	{
 		printf("%d is negitive\n", n);
 	}
 	else
-		printf("%d is zero\n", n);
+		printf("%d is positive\n", n);
 
 	return (0);
 }
