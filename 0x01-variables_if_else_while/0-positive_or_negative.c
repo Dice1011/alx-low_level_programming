90222b341cb13be39b129e9c828d395efd4e7fcd
Dice1011 <u12283593@tuks.co.za>
2023-06-18 15:20:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ int main(void)
 
 	if (n == 0)
 	{
-		printf("%d : is zero\n", n);
+		printf("%d \: is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negitive\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
