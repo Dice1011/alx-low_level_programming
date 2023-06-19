350fde7b7cbd09a6ce88ad1c3aabfe489d63e9e4
Dice1011 <u12283593@tuks.co.za>
2023-06-18 18:36:04 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Program prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 9)
+				continue;
+		}
+		putchar ((i % 10) + '0');
+		putchar (',');
+	}
+	putchar ('\n');
+	return (0);
+}
