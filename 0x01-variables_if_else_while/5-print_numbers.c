963efb9634b5fc6a07ad60728b9ed2b7ed55a1e1
Dice1011 <u12283593@tuks.co.za>
2023-06-18 17:07:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Scripts prints numbers
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
