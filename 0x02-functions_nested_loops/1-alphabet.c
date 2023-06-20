5fbec6e2c859349be7cf237e248abbd1e82eaa73
Dice1011 <u12283593@tuks.co.za>
2023-06-20 07:54:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints alpahabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a', alpha <= 'z', alpha++)
+	{
+		_putchar(alpha);
+		_putchar('\n');
+	}
+}
