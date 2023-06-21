acb0ed451b3568a63d31082d7685d5349e4337e8
Dice1011 <u12283593@tuks.co.za>
2023-06-21 08:28:21 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Program prints alphabets 10 times
+ *
+ * Return: Void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ac;
+	int i = 0;
+
+	while(i < 10)
+	{
+		for (ac = 'a'; ac <= 'z'; ac++)
+		{
+			_putchar(ac);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
