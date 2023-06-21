39bbffd2f37c2d4b1f1955164a14e3b1a06f8495
Dice1011 <u12283593@tuks.co.za>
2023-06-21 16:04:28 -0700

    3-islower.c debugged

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - Program checks for lower case character
+ *_islower - Program checks for lower case character
+ *@a: is the function parameter
  *
  * Return: i for lower case and 0 if other
  */
 
-int is_slower(int a)
+int _islower(int a)
 {
 	if (a >= 97 && a <= 122)
 	{
