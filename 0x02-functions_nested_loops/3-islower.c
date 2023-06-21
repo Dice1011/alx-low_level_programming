36a39c31d4b23f6088cb6a99f3de8ed66cdc304d
Dice1011 <u12283593@tuks.co.za>
2023-06-21 08:43:14 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Program checks for lower case character
+ *
+ * Return: i for lower case and 0 if other
+ */
+
+int is_slower(int a)
+{
+	if (a >= 97 && a <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
