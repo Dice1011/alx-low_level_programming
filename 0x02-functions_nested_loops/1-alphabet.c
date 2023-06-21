11415c0af6530b905096ed29b14207e5fd513f1f
Dice1011 <u12283593@tuks.co.za>
2023-06-21 15:49:42 -0700

    1-alphabet.c debugged

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet - writes the character to stdout
- *
- * Description: print_alphabet prints char inlower case
- *
- * @A: The character to print
- *
- * Return: void
+ * print_alphabet - writes the character in lower case
  */
-
 void print_alphabet(void)
 {
 	char A;
 
 	for (A = 'a'; A <= 'z'; A++)
-		_putchar(A);
-
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
