d9eff4caa90b8dab937b0fa7a88b15f15900b886
Dice1011 <u12283593@tuks.co.za>
2023-06-21 08:56:00 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * function that checks for alphabetic character
+ *
+ * Return: 1 or alpha character or 0 for other
+ */
+
+int _isalpha(int a)
+{
+	if ((a >= 65 && c <= 97) || (a >= 97 && a <= 122))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
