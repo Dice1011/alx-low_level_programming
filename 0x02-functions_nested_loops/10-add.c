4a89b8a0cbfc268c6f1832384ba71f201d3cc69d
Dice1011 <u12283593@tuks.co.za>
2023-06-21 22:34:00 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function adds two integers
+ *
+ * @a: parameter 1
+ * @b: Parameter 2
+ *
+ * Return: Integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b); 
+
