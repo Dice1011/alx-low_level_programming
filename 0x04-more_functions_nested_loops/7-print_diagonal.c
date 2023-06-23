ea3b6f0c8dbf1003ba63b262e835d5eb7fe09ab2
Dice1011 <u12283593@tuks.co.za>
2023-06-23 08:39:57 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - function draws diagonal line
+ * @n: parameter 1
+ * Return: integer
+ */
+void print_diagonal(int n)
+{
+	int slash, spc;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (slash = 1; slash <= n; slash++)
+		{
+			for (spc = 1; spc <= slash; spc++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
