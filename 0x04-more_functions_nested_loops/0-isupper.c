8d61d2aff61bbec7185ce1ef4cf3d03831e59a1b
Dice1011 <u12283593@tuks.co.za>
2023-06-23 02:46:32 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,2 +1,14 @@
 #include "main.h"
+/**
+ * _isupper - checks if charcter is upper/lower case
+ * @c: designated variable
+ * Return: integer
+ */
+int _isupper(int c);
+{
+	if(c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
 
