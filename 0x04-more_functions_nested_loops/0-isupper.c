c65b064d7a8ecd70eac49fe0327f04eab06abfe2
Dice1011 <u12283593@tuks.co.za>
2023-06-23 03:41:39 -0700

    0-isupper.c debugged

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isupper - checks if charcter is upper/lower case
+ * _isupper - checks if charcter is upper
  * @c: designated variable
- * Return: integer
+ * Return: 1 if upper 0 if else
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 65) && (c <= 90))
 		return (1);
 	else
 		return (0);
