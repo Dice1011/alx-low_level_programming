d5394555d95e69b9fa59a9a2c8a33ae797daa9c6
Dice1011 <u12283593@tuks.co.za>
2023-06-23 04:25:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - function prints numbers from 1 - 9
+ *@i: number
+ *Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('i');
+	}
+	_puchar('\n');
+	return 0;
+}
