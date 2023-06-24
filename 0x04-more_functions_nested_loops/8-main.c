aec29c30e27da2e1488764b03cddb8676b220d8d
Dice1011 <u12283593@tuks.co.za>
2023-06-24 02:53:31 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -5,9 +5,8 @@
  */
 int main(void)
 {
-	print_diagonal(0);
-	print_diagonal(9);
-	print_diagonal(98);
-	print_diagonal(-4);
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	return (0);
 }
