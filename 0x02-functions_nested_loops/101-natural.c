11c6b5444fe1ce1131bed1c20365deb631e3390e
Dice1011 <u12283593@tuks.co.za>
2023-06-24 23:19:20 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,10 +7,13 @@ int main(void)
 {
 	int i, sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	while (i < 1024)
 	{
 		if ((i % 3) == 0 && (i % 5))
+		{
 			sum += i;
+		}
+	i++;
 	}
 	printf("%d\n", sum);
 	return (0);
