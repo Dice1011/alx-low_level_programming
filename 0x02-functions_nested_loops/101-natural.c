1bdec2eb31229ed9b2cc2ac62672d9a8a66cbf8e
Dice1011 <u12283593@tuks.co.za>
2023-06-24 23:28:32 -0700

    101-natural.c debugged

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i = 0;
+	int sum = 0;
 
 	while (i < 1024)
 	{
-		if ((i % 3) == 0 && (i % 5))
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum += i;
 		}
