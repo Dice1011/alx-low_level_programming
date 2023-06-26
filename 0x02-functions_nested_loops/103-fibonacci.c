a681e25f2e1911d2619c3ea56e33092f5caad9da
Dice1011 <u12283593@tuks.co.za>
2023-06-26 05:07:45 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - program prins fibbo values from 1 - 4000000
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	float total;
+	unsigned long Fib1 = 0, Fib2 = 1, sum;
+
+	while (1)
+	{
+		sum = Fib1 + Fib2;
+
+		if (sum > 4000000)
+		{
+			break;
+		}
+		if ((sum % 2) == 0)
+		{
+			total += sum;
+		}
+		Fib1 = Fib2;
+		Fib2 = sum;
+	}
+	printf("%f\n", total);
+
+	return (0);
+}
