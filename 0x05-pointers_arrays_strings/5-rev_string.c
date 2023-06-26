e716d4200e091e3e6776b659859b56e74c4b3985
Dice1011 <u12283593@tuks.co.za>
2023-06-26 08:58:27 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string -reverses a string
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 1, ind = 1;
+	char temp;
+	
+	while (s[ind++])
+	{
+		len++
+	}
+	for (ind = len - 1; ind >= len / 2; ind--)
+	{
+		temp = s[ind];
+		s[ind] = s[len - ind - 1];
+		s[len - ind - 1] = temp
+	}
+}
