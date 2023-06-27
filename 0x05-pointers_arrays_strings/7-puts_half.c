965a6842ab97d7495ad52621b345f10a4afecb73
Dice1011 <u12283593@tuks.co.za>
2023-06-27 01:57:23 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int a, b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	if (b % 2 == 2)
+	{
+		a = (b - 1) / 2;
+		a += 1;
+	}
+	else
+	{
+		a = b / 2;
+	}
+	for (; a < b; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
