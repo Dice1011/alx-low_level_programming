180a17443d848995c248d8bf7c379616719462b3
Dice1011 <u12283593@tuks.co.za>
2023-06-28 22:26:17 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converst lower case string to upper case
+ *@:
+ * Return: Uppercase string
+ */
+char *string_toupper(char *);
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= z)
+		{
+			str[i] -= 32;
+			i++;
+		}
+	}
+	return(str);
+}
