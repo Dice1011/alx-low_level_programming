fa5c2f57aa5d449abbf2b46971d94e704b5ac19d
Dice1011 <u12283593@tuks.co.za>
2023-06-30 16:19:17 -0700

    5-string_toupper.c debugged

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,14 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		if (str[i] >= '0' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
-			i++;
 		}
+		i++;
+		
 	}
 	return (str);
 }
