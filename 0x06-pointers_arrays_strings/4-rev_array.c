77e94ae4fbe6110b6055cf906b81d463d962a745
Dice1011 <u12283593@tuks.co.za>
2023-06-30 14:57:01 -0700

    4-rev_array.c debugged

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,10 @@ void reverse_array(int *a, int n)
 {
 	int rev, i;
 
-	for (i = n - 1; i >= n / 2; i++)
+	for (i = 0; i < n--; i++)
 	{
-		rev = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
-		a[i] = rev;
+		rev = a[i];
+		a[i] = a[n];
+		a[n] = rev;
 	}
 }
