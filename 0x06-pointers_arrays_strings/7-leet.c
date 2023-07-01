f22dc1a3240e1dda54bb5d21855737e5100d7da2
Dice1011 <u12283593@tuks.co.za>
2023-06-30 17:02:42 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes string into 1337
+ * @str: string
+ * Return: string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char A1[] = "aAeEoOtTLl";
+	char B1[] = "4433007711";
+
+	for (i = 0; str[i] != '\0';; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == A1[j])
+			{
+				A1[i] = B1[j];
+			}
+		}
+	}
+	return (str);
+}
