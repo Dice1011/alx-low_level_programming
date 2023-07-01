b2a4549faefa61112f876b9a5d00b535e7894a2f
Dice1011 <u12283593@tuks.co.za>
2023-06-30 17:08:25 -0700

    7-leet.c debugged

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *str)
 	char A1[] = "aAeEoOtTLl";
 	char B1[] = "4433007711";
 
-	for (i = 0; str[i] != '\0';; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
