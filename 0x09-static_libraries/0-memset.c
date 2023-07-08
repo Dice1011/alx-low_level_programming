67a6fb01afffba514fd66acd27125ff9f143646a
Dice1011 <u12283593@tuks.co.za>
2023-07-08 00:53:18 -0700

    0-memset.c file creation

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,18 @@
 #include "main.h"
+/**
+ * _memset - function fills memory with a constant byte
+ * @s: Pointer to memory
+ * @b: constant byte
+ * @n: bytes
+ * Return: pointer to memory s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsighned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return(s);
 }
