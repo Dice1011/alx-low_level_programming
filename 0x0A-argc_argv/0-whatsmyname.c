80970d65f66499b24b6d1ffac44ca4a79f111a7d
Dice1011 <u12283593@tuks.co.za>
2023-07-07 18:02:38 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,8 @@
  * @argc: number of arguments
  * @argv: Array
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	printf("%a\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
