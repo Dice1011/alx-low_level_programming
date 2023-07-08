c022c603e77fc4c3f1a5ef54f0268705679ef201
Dice1011 <u12283593@tuks.co.za>
2023-07-07 18:37:11 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array
+ * Return:Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int j;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (j = 0; *argv; j++, argv++)
+
+		printf("%d\n", j - 1);
+	}
+	return (0);
+}
+
