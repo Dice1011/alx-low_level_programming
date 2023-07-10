674f53b466432d4b3eda61f278ff8e2a7d541d31
Dice1011 <u12283593@tuks.co.za>
2023-07-09 18:59:34 -0700

    5 scrippts from 0x07 coded for library creation

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,5 +1,27 @@
 #include "main.h"
+/**
+ * _strspn - gets the lenght of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: The number of bytes in initial segment of s
+ */
 unsigned int _strspn(char *s, char *accept)
 {
+	int i;
 
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (**s == accept[i])
+			{
+				i++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (0);
+		}
+		s++;
+	}
+	return (0);
 }
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,5 +1,26 @@
 #include "main.h"
+/**
+ * _strstr - function locates a substring
+ * @haystack: String
+ * @needle: Substring
+ * Return: pointer to beginning of substring or 0 if none
+ */
 char *_strstr(char *haystack, char *needle)
 {
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *A = haystack;
+		char *B = needle;
 
+		while (*A == *B && *B != '\0')
+		{
+			A++;
+			B++;
+		}
+		if (*B == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
 }
