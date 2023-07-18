445c66583558d547251ccbcdc8700daf554e1449
Dice1011 <u12283593@tuks.co.za>
2023-07-18 04:55:08 -0700

    2-print_dog.c debugged

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,14 +7,14 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->name : "(nil)");
-	}
-	else
-	{
-		return;
 	}
 }
