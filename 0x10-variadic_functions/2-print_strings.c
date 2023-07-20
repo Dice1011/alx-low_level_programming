73729de20e72217ad27664a5e46942e4cf2660ca
Dice1011 <u12283593@tuks.co.za>
2023-07-20 05:37:25 -0700

    all tasks done and completed

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_finctions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints string
+ i* @n: number of strings passed
+ * @...:variable number of strings to print
+ * @separator: string to print between strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	char *s;
+	unsigned int i;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(strings);
+}
+
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include "variadic_functiions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int);
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double);
+					break;
+				case 's':
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
