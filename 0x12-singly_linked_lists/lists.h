1558fa68b6bd2fd2a1318fe7eee26a6fd5911bdf
Dice1011 <u12283593@tuks.co.za>
2023-07-28 23:01:46 -0700

    structure addition to headerfile

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct list_s - sinly linked list
+ * @str: memory allocated string
+ * @len: length of the string
+ * @next: pointer to next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
