3637a004966b4bdd40a1453bf93c8dd59025412b
Dice1011 <u12283593@tuks.co.za>
2023-07-28 23:20:12 -0700

    0-print_list.c completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,27 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * 
+ * print_list - prints all elements of list_t list
+ *@h: list_t
+ *Return: number of nodes in h
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
