d4fdf38510ccf882147ffc37605416c26f649597
Dice1011 <u12283593@tuks.co.za>
2023-07-29 13:11:28 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib>
+/**
+ *free_list - frees a linked list
+ *@head: linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while ((node = head) != NULL)
+	{
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
