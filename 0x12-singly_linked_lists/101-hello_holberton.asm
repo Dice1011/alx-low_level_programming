c56ad5f51df64af9ae2d5fb286e429f634902f2a
Dice1011 <u12283593@tuks.co.za>
2023-07-31 02:52:14 -0700

    101-hello_holberton.asm comments added

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,23 @@
-	extern printf
+	extern printf			;C function to call
 
-	section .data
-msg:	db "Hello, Holberton",0
-fmt:	db "%s", 10, 0
+	section .data			;Data section, initializing variables
+msg:	db "Hello, Holberton",0		;C string, needs 0 at end
+fmt:	db "%s", 10, 0			;Printf format
 
-	section .text
+	section .text			;Code section
 
-	global main
+	global main			;Standard gcc entry point
 
-main:
-	push rbp
+main:					;Program label entry for entry pint
+	push rbp			;set up stack frame
 
 	mov rdi,fmt
 	mov rsi,msg
-	mov rax,0
-	call printf
+	mov rax,0			;Can be  xor  rax,rax
 
-	pop rbp
+	call printf			;Call C function
 
-	mov rax,0
-	ret
+	pop rbp				;Restore stack
+
+	mov rax,0			;Normal, no error, return alue
+	ret				;Return
