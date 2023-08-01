af9b6921775df86c18aaae7b5311dce67b94abbc
Dice1011 <u12283593@tuks.co.za>
2023-08-01 03:54:39 -0700

    Task 4 and 5 completed

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint - frees linked list
+ * @head: pointer to linked list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *old_node;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	whie (*head)
+	{
+		old_node = (*head)->next;
+		free(head);
+		*head = old_node;
+	}
+	*head = NULL;
+}
